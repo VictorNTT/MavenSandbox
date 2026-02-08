ac141d7a4bd3c30f3e35c63190807880c8368cb8
Victor Nguyen <intelteq@hotmail.com>
2026-02-08 06:41:08 -0500

    introduced KafkaTemplate (vs KafkaProducer)

--- SpringbootMVC/src/main/java/org/example/kafka/KafkaTestApp.java
+++ SpringbootMVC/src/main/java/org/example/kafka/KafkaTestApp.java
@@ -3,25 +3,30 @@ package org.example.kafka;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 // Start Kafka server before running this app
 // C:\kafka\bin\windows\kafka-server-start.bat C:/kafka/config/server.properties
 
-@Service
+@Service // @Service - comment this line out to turn off this class from running
 public class KafkaTestApp implements CommandLineRunner {
 
     static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
@@ -33,7 +38,8 @@ public class KafkaTestApp implements CommandLineRunner {
     private String topic;
 
     private KafkaConsumer<String, String> kafkaConsumer;
-    private KafkaProducer<String, String> kafkaProducer;
+   // private KafkaProducer<String, String> kafkaProducer;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Override
     public void run(String... args) throws Exception {
@@ -47,8 +53,13 @@ public class KafkaTestApp implements CommandLineRunner {
         kafkaConsumer.subscribe(Arrays.asList(topic));
 
         // Register a producer
-        System.out.println("KafkaProducer - registering...");
-        kafkaProducer = new KafkaProducer<>(connMap);
+       // System.out.println("KafkaProducer - registering...");
+       // kafkaProducer = new KafkaProducer<>(connMap);
+
+        // Register a producer using kafkaTemplate
+
+        System.out.println("KafkaTemplate - registering...");
+        kafkaTemplate = createKafkaTemplate(connMap);
     }
 
     // This task running indefinitely to consume messages from the topic
@@ -73,7 +84,8 @@ public class KafkaTestApp implements CommandLineRunner {
     // fixedDelay - how often to send a message
     @Scheduled (initialDelay = 1000, fixedDelay = 1000)
     public void simulateKafkaProducerProcess() throws Exception {
-        if(kafkaProducer == null) {
+//        if(kafkaProducer == null) {
+        if(kafkaTemplate == null) {
             System.err.println("kafkaProducer not initialized");
             return;
         }
@@ -82,16 +94,47 @@ public class KafkaTestApp implements CommandLineRunner {
             String data = "Test message";
             StringRawData srd = new StringRawData(id, data);
             ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, id +"", srd.toString());
-            kafkaProducer.send(record, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata recordMetadata, Exception ex) {
-                    if(ex != null) {
-                        System.err.println("Error: " + ex);
-                    }
+
+// Use KafkaTemplate instead of KafkaProducer
+//            kafkaProducer.send(record, new Callback() {
+//                @Override
+//                public void onCompletion(RecordMetadata recordMetadata, Exception ex) {
+//                    if(ex != null) {
+//                        System.err.println("Error: " + ex);
+//                    }
+//                }
+//            });
+
+            CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(record);
+            future.whenComplete((result, ex) -> {
+                if (ex == null) {
+                    //handleSuccess(record);
+                    System.out.println(
+                            dateFormat.format(Calendar.getInstance().getTime())
+                            + " Successfully sent");
+                }
+                else {
+                    //handleFailure(record, ex);
+                    System.err.println(
+                            dateFormat.format(Calendar.getInstance().getTime())
+                            + " Failed sending");
                 }
             });
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    // KafkaTemplate is an api wrapper of KafkaProducer; you can use either KafkaTemplate or directly KafkaProducer
+
+    public KafkaTemplate<String, String> createKafkaTemplate(Map<String, Object> config) {
+        return new KafkaTemplate<>(producerFactory(config));
+    }
+
+    // Create a ProducerFactory to produce  KafkaTemplate's
+    @Bean
+    public ProducerFactory<String, String> producerFactory(Map<String, Object> config) {
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
 }
